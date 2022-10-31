33f553fa9bb2778ac1bdf9936c0195072ae4b01c
yyboom <yoonyoung37@naver.com>
2022-10-31 14:41:05 +0900

    first project-choyunyoung

---
+++ src/test/java/com/mogotco/MogotcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogotco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogotcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
