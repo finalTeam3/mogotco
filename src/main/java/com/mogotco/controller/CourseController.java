c91dfef8e9d5a496e839415c90b6ca8d9d1bf026
parkpirate <skbsy153@gmail.com>
2022-11-08 17:35:07 +0900

    골뱅이 작업 완료~ 2022.11.08_혜정

--- src/main/java/com/mogotco/controller/CourseController.java
+++ src/main/java/com/mogotco/controller/CourseController.java
@@ -5,16 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/courses")
 public class CourseController {
-	
-	String dir = "courses/";
-	
-	@RequestMapping("")
-	public String course(Model model) {
-		model.addAttribute("center", "courses");
-		return "index";
-	}
 	
 	@RequestMapping("/course")
 	public String courses(Model model) {
