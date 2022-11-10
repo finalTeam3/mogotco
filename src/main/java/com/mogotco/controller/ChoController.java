b27f471bfab221e78f17f71a5c2abba1b6676516
yyboom <yoonyoung37@naver.com>
2022-11-10 11:46:08 +0900

    order관련 페이지 폴더화

--- src/main/java/com/mogotco/controller/ChoController.java
+++ src/main/java/com/mogotco/controller/ChoController.java
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ChoController {
 	
+	String order = "order/";
+	
 	@RequestMapping("/order")
 	public String order(Model model) {
-		model.addAttribute("center", "order");
+		model.addAttribute("center", order+"order");
 		return "index";
 	}
 	
 	@RequestMapping("/kg")
 	public String kg() {
-		return "kg";
+		return order+"kg";
 	}
 	
 	@RequestMapping("/main")
