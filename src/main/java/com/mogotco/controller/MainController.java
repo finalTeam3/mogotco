0bf57fd5889c21d64044c9ca69eb992e1c22a832
parkpirate <skbsy153@gmail.com>
2022-11-15 13:55:07 +0900

    화면 연결 완료_2022.11.15

--- src/main/java/com/mogotco/controller/MainController.java
+++ src/main/java/com/mogotco/controller/MainController.java
@@ -1,12 +1,21 @@
 package com.mogotco.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mogotco.dto.BoardDTO;
+import com.mogotco.service.BoardService;
+
 @Controller
 @RequestMapping("/")
 public class MainController {
+	
+	@Autowired
+	BoardService board_service;
 	
 	@RequestMapping("")
 	public String main() {
@@ -16,7 +25,14 @@ public class MainController {
 	//자주 묻는 질문 페이지
 	@RequestMapping("/faq")
 	public String faq(Model model) {
-		model.addAttribute("center", "faq");
+		List<BoardDTO> board = null;
+		try {
+			board = board_service.selectboardtype(2);
+			model.addAttribute("faq", board);
+			model.addAttribute("center", "faq");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return "main";
 	}
 	
