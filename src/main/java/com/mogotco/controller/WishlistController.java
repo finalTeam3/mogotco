c37b3ed53bad05672328384337cdf501ca06dd65
seonghoon <shee5032@naver.com>
2022-11-21 16:41:00 +0900

    wishlist 페이지 작업중 2

--- src/main/java/com/mogotco/controller/WishlistController.java
+++ src/main/java/com/mogotco/controller/WishlistController.java
@@ -1,8 +1,14 @@
 package com.mogotco.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mogotco.dto.WishlistDTO;
+import com.mogotco.service.WishlistService;
 
 @Controller
 @RequestMapping("/wishlist")
@@ -10,10 +16,22 @@ public class WishlistController {
 	
 	String wishlist = "wishlist/";
 	
+	@Autowired
+	WishlistService wservice;
+	
 	//wishlist페이지
 	@RequestMapping("/wishlist")
-	public String wishlist(Model model) {
-		model.addAttribute("center", wishlist+"wishlist");
+	public String wishlist(Model model, String id) {
+		List<WishlistDTO> wlist = null;
+		try {
+			wlist = wservice.wmentor(id);
+			model.addAttribute("wlist",wlist);
+			model.addAttribute("center", wishlist+"wishlist");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return "main";
 	}
 	
