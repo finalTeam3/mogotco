41d5fd79f325af4ad7d05fb1f7970e2fef859a21
parkpirate <skbsy153@gmail.com>
2022-11-21 17:12:46 +0900

    로그인 모달창 완료_2022.11.21 혜정

--- src/main/java/com/mogotco/controller/UserController.java
+++ src/main/java/com/mogotco/controller/UserController.java
@@ -51,16 +51,15 @@ public class UserController {
 		try {
 			user = user_service.get(id);
 			if(user == null) {
+				model.addAttribute("logresult", "f");
 				model.addAttribute("center", dir + "login");
-				model.addAttribute("logresult", "fail");
 			} else {
 				if(pwd.equals(user.getUserpwd())) {
+					model.addAttribute("logresult", "t");
 					session.setAttribute("loginuser", user);
-					model.addAttribute("logresult", "s");
-					maincenter(model);
 				} else {
+					model.addAttribute("logresult", "f");
 					model.addAttribute("center", dir + "login");
-					model.addAttribute("logresult", "fail");
 				}
 			}
 		} catch (Exception e) {
