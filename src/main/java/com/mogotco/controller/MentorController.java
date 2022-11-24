7020dcc7c4fa5ef2359b6511e4d0902bd7d178db
yyboom <yoonyoung37@naver.com>
2022-11-24 09:51:14 +0900

    주석

--- src/main/java/com/mogotco/controller/MentorController.java
+++ src/main/java/com/mogotco/controller/MentorController.java
@@ -22,55 +22,55 @@ import com.mogotco.service.MentorService;
 import com.mogotco.service.MentoringService;
 import com.mogotco.service.UserService;
 
-
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
-	
+
 	String mentor = "mentor/";
-	
+
 	@Autowired
 	MentorService mservice;
 
 	@Autowired
 	MentoringService mtiservice;
-	
+
 	@Autowired
 	UserService uservice;
-	
+
 	@Autowired
 	MWishcateService mwservice;
 
 	@Value("${admindir}")
 	String admindir;
-	
+
 	@Value("${userdir}")
 	String userdir;
-	
-	//아이디값 유무 판단
+
+	// 아이디값 유무 판단
 	@RequestMapping("/idcheck")
-	public void idcheck(int mentorid,HttpServletRequest request, HttpServletResponse response) {
-		HttpSession session = request.getSession(false);	
-		if(session == null) {
-					try {
-						response.sendRedirect("/mogotco/mentor/nonid?mentorid="+mentorid);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}//controller주소로 감
-				}else {
-					try {
-						response.sendRedirect("/mogotco/mentor/mentordetail?mentorid="+mentorid);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}//controller주소로 감
-				}
+	public void idcheck(int mentorid, HttpServletRequest request, HttpServletResponse response) {
+		// current session이 없으면 없는채로 두는 것
+		HttpSession session = request.getSession(false);
+		// session정보가 없을 때
+		if (session == null) {
+			try {
+				// session이 없을 때 controller주소로 감
+				response.sendRedirect("/mogotco/mentor/nonid?mentorid=" + mentorid);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-	
-	
-	
-	//멘토 상세페이지
+		} else {
+			try {// session이 있을 때 controller주소로 감
+				response.sendRedirect("/mogotco/mentor/mentordetail?mentorid=" + mentorid);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 멘토 상세페이지
 	@RequestMapping("/mentordetail")
 	public String mentordetail(Model model, int mentorid) {
 		MentorDTO mta = null;
@@ -78,24 +78,24 @@ public class MentorController {
 		List<MentorDTO> mtlist = null;
 		List<MWishcateDTO> mwclist = null;
 		try {
-					mta = mservice.get(mentorid);
-					mtd = mservice.mentordetail(mentorid);
-					mtlist = mservice.mentoritem(mentorid);
-					mwclist = mwservice.mwcate(mentorid);
-					model.addAttribute("mta",mta);
-					model.addAttribute("mtd",mtd);
-					model.addAttribute("mtlist",mtlist);
-					model.addAttribute("mwclist",mwclist);
-					model.addAttribute("center", mentor+"mentordetail");
-				
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			mta = mservice.get(mentorid);
+			mtd = mservice.mentordetail(mentorid);
+			mtlist = mservice.mentoritem(mentorid);
+			mwclist = mwservice.mwcate(mentorid);
+			model.addAttribute("mta", mta);
+			model.addAttribute("mtd", mtd);
+			model.addAttribute("mtlist", mtlist);
+			model.addAttribute("mwclist", mwclist);
+			model.addAttribute("center", mentor + "mentordetail");
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "main";
 	}
-	
-	//비회원 멘토 상세 페이지
+
+	// 비회원 멘토 상세 페이지
 	@RequestMapping("/nonid")
 	public String nonid(Model model, int mentorid) {
 		MentorDTO mta = null;
@@ -103,64 +103,64 @@ public class MentorController {
 		List<MentorDTO> mtlist = null;
 		List<MWishcateDTO> mwclist = null;
 		try {
-				mta = mservice.get(mentorid);
-				mtd = mservice.mentordetail(mentorid);
-				mtlist = mservice.mentoritem(mentorid);
-				mwclist = mwservice.mwcate(mentorid);
-				model.addAttribute("mta",mta);
-				model.addAttribute("mtd",mtd);
-				model.addAttribute("mtlist",mtlist);
-				model.addAttribute("mwclist",mwclist);
-				model.addAttribute("center", mentor+"mentordetail1");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		return "main";
-	}
-	
-	//멘토 수정페이지
-	@RequestMapping("/mentormodify")
-	public String mentormodify(Model model, String id) {
-		MentorDTO mentorall = null;
-		try {
-			mentorall = mservice.mentorAll(id);
-			model.addAttribute("m",mentorall);
-			model.addAttribute("center", mentor+"mentormodify");
+			mta = mservice.get(mentorid);
+			mtd = mservice.mentordetail(mentorid);
+			mtlist = mservice.mentoritem(mentorid);
+			mwclist = mwservice.mwcate(mentorid);
+			model.addAttribute("mta", mta);
+			model.addAttribute("mtd", mtd);
+			model.addAttribute("mtlist", mtlist);
+			model.addAttribute("mwclist", mwclist);
+			model.addAttribute("center", mentor + "mentordetail1");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return "main";
 	}
-	
+
+	// 멘토 수정페이지
+	@RequestMapping("/mentormodify")
+	public String mentormodify(Model model, String id) {
+		MentorDTO mentorall = null;
+		try {
+			mentorall = mservice.mentorAll(id);
+			model.addAttribute("m", mentorall);
+			model.addAttribute("center", mentor + "mentormodify");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "main";
+	}
+
 	// 멘토 정보 업데이트 기능
 	@RequestMapping("/modifyimpl")
 	public String update(Model model, MentorDTO mentordto) {
 
 		String mpimgname = mentordto.getMpimg().getOriginalFilename();
 		mentordto.setMentorimg(mpimgname);
-		
+
 		String mcimgname = mentordto.getMcimg().getOriginalFilename();
-		mentordto.setMcardimg(mcimgname);		
-		
+		mentordto.setMcardimg(mcimgname);
+
 		try {
 			Util.saveMentorFile(mentordto.getMpimg(), mentordto.getMcimg(), admindir, userdir);
 			mservice.modify(mentordto);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "redirect:mentormodify?id="+mentordto.getUserid();
-	}	
-	
-	//멘토 등록페이지
+		return "redirect:mentormodify?id=" + mentordto.getUserid();
+	}
+
+	// 멘토 등록페이지
 	@RequestMapping("/mentorregister")
 	public String mentorregister(Model model, String id) {
 		UserDTO user = null;
 		try {
 			user = uservice.get(id);
 			model.addAttribute("u", user);
-			model.addAttribute("center", mentor+"mentorregister");
+			model.addAttribute("center", mentor + "mentorregister");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -171,20 +171,20 @@ public class MentorController {
 	// 멘토 정보 등록 기능 (멘토 지원)
 	@RequestMapping("/registerimpl")
 	public String register(Model model, MentorDTO mentordto) {
-		
+
 		String mpimgname = mentordto.getMpimg().getOriginalFilename();
 		mentordto.setMentorimg(mpimgname);
-		
+
 		String mcimgname = mentordto.getMcimg().getOriginalFilename();
 		mentordto.setMcardimg(mcimgname);
-		
+
 		try {
 			Util.saveMentorFile(mentordto.getMpimg(), mentordto.getMcimg(), admindir, userdir);
 			mservice.register(mentordto);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "redirect:mentorregister?id="+mentordto.getUserid();
-	}		
-	
+		return "redirect:mentorregister?id=" + mentordto.getUserid();
+	}
+
 }
