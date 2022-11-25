3fe92933ff019ac3f1d344ba5fb9ab08de26beb0
parkpirate <skbsy153@gmail.com>
2022-11-25 23:32:18 +0900

    충돌해결완료_2022.11.25 혜정

--- src/main/java/com/mogotco/controller/AjaxController.java
+++ src/main/java/com/mogotco/controller/AjaxController.java
@@ -3,14 +3,8 @@ package com.mogotco.controller;
 import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Value;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> refs/heads/hyejung
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
