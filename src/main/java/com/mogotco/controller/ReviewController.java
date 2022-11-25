abf5785833ca16ac0ea5d1119398077f4a542364
parkpirate <skbsy153@gmail.com>
2022-11-26 00:45:07 +0900

    구매이력에서 리뷰 넘어갈때 멘토아이디 넘기는중_2022.11.26 혜정

--- src/main/java/com/mogotco/controller/ReviewController.java
+++ src/main/java/com/mogotco/controller/ReviewController.java
@@ -1,19 +1,33 @@
 package com.mogotco.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mogotco.dto.MentorDTO;
+import com.mogotco.service.MentorService;
 
 @Controller
 @RequestMapping("/review")
 public class ReviewController {
 	
-	String review = "review/";
+	@Autowired
+	MentorService mentor_service;
+	
+	String dir = "review/";
 	
 	//리뷰페이지
 	@RequestMapping("/review")
-	public String review(Model model) {
-		model.addAttribute("center", review+"review");
+	public String review(Model model, int mentorid) {
+		MentorDTO mentor = null;
+		try {
+			mentor = mentor_service.get(mentorid);
+			model.addAttribute("reviewmentor", mentor);
+			model.addAttribute("center", "mentor/mentordetail");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+		}
 		return "main";
 	}
 	
