b3c58272f3113cc1770a075fadd212257fe93271
YunYoungCho <yoonyoung37@naver.com>
2022-12-05 04:10:13 +0900

    이전페이지 다음페이지는 됨 1,2,3,4,5페이지 별로는 아직 해결 못함

--- src/main/java/com/mogotco/mapper/MentoringMapper.java
+++ src/main/java/com/mogotco/mapper/MentoringMapper.java
@@ -12,9 +12,9 @@ import com.mogotco.frame.MyMapper;
 @Mapper
 public interface MentoringMapper extends MyMapper<Integer, MentoringDTO> {
 	// 등록된 모든 멘토링을 출력하기 위한 mapper추가
-	public List<MentoringDTO> viewMentoringAll() throws Exception;
+	public List<MentoringDTO> viewMentoringAll(Integer meningnum) throws Exception;
 	// 등록된 멘토링 리스트 출력을 위한 mapper추가
-	public List<MentoringDTO> selectMentoringAll(String mname) throws Exception;
+	public List<MentoringDTO> selectMentoringAll(String mname, Integer meningnum) throws Exception;
 	// 멘토링 옵션 출력을 위한 mapper 추가
 	public MentoringDTO viewMentoringOp(int mentoringid) throws Exception;
 	//지금 당장 롸잇나우 할 수 있는 멘토링
